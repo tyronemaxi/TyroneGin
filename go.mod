2a0ef0aa1ee063356283995851889be3a2908a37
田志超 <tyronemaxi@163.com>
2021-11-17 22:47:06 +0800

    main 函数添加

---
+++ go.mod
@@ -0,0 +1,5 @@
+module TyroneGin
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.4
---
+++ route/route.go
@@ -0,0 +1,9 @@
+package route
+
+import "github.com/gin-gonic/gin"
+
+func IniteRoute(g *gin.Engine) {
+	g.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+}
